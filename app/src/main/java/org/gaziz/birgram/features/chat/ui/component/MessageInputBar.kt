75ce134e0e608d3540e41590b497b50529a1916f
GAZIZlikesLollipop <gazizvalitov230378@gmail.com>
2026-09-10 21:02:14 +0500

    feat(chat): add navigationBarsPadding modifier in MessageInputBar

--- app/src/main/java/org/gaziz/birgram/features/chat/ui/component/MessageInputBar.kt
+++ app/src/main/java/org/gaziz/birgram/features/chat/ui/component/MessageInputBar.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Icon
@@ -60,6 +61,7 @@ fun MessageInputBar(
         modifier = modifier
             .fillMaxWidth()
             .background(containerColor)
+            .navigationBarsPadding()
     ) {
         Row(
             verticalAlignment = Alignment.CenterVertically
